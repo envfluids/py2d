047c3215b168babcf8898fc0a72d1a871e67e300
rm99 <rmojgani@gmail.com>
2023-07-07 14:06:36 -0500

    cotourf compare

--- examples/run_solver.py
+++ examples/run_solver.py
@@ -3,17 +3,35 @@ from py2d.Py2D_solver import Py2D_solver
 # Script to call the function with the given parameters
 Py2D_solver(Re=20e3, # Reynolds number
                fkx=4, # Forcing wavenumber in x
-               fky=0, # Forcing wavenumber in y
+               fky=4, # Forcing wavenumber in y
                alpha=0.1, # Rayleigh drag coefficient
-               beta=0, # Coriolis parameter
+               beta=20, # Coriolis parameter
                NX=128, # Number of grid points in x and y '32', '64', '128', '256', '512'
                SGSModel_string='DLEITH', # SGS model to use 'NoSGS', 'SMAG', 'DSMAG', 'LEITH', 'DLEITH', 'PiOmegaGM2', 'PiOmegaGM4', 'PiOmegaGM6'
                eddyViscosityCoeff=0.17, # Coefficient for eddy viscosity models: SMAG and LEITH
                dt=5e-4, # Time step
                saveData=True, # Save data
-               tSAVE=1, # Time interval to save data
-               tTotal=10, # Total time of simulation
+               tSAVE=10, # Time interval to save data
+               tTotal=50, # Total time of simulation
                readTrue=False, 
                ICnum=1, # Initial condition number: Choose between 1 to 20
                resumeSim=False, # tart new simulation (False) or resume simulation (True) 
                jobName='')
+#%%
+import scipy.io as sio
+# directory = '/home/rm99/Mount/py2d/examples/results/Re20k_fkx4fky0_r0.1/DLEITH_/NX128/dt0.0005_IC1/data/'
+directory = '/home/rm99/Mount/py2d/examples/results/Re20k_fkx4fky4_r0.1/DLEITH_/NX128/dt0.0005_IC1/data/'
+
+filename = '5.mat'
+mat_contents = sio.loadmat(directory+filename)
+#%%
+mat_contents.keys()
+Omega = mat_contents['Omega']
+#%%
+plt.contourf(Omega,cmap='bwr',levels=110)
+#%%
+plt.pcolor(Omega)
+
+#%%
+plt.contourf(X,Y,Omega,cmap='bwr',levels=110)
+
---
+++ examples/test_derivatives.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 12:26:44 2023
+
+@author: rm99
+Test of derivatives
+"""
+import sys
+sys.path
+sys.path.append('../')
+sys.path.append('/home/rm99/Mount/py2d')
+# Import os module
+import os
+from pathlib import Path
+
+# Import Python Libraries
+from prettytable import PrettyTable
+import jax
+from jax import jit
+import numpy as nnp
+import jax.numpy as np
+from scipy.io import loadmat, savemat
+import time as runtime
+from timeit import default_timer as timer
+print(jax.default_backend())
+print(jax.devices())
+
+# Import Custom Module
+from py2d.convection_conserved import *
+from py2d.convert import *
+from py2d.aposteriori_analysis import eddyTurnoverTime_2DFHIT
+from py2d.SGSModel import *
+# from py2d.uv2tau_CNN import *
+from py2d.initialize import *
+
+
+
+
+from py2d.derivative_2DFHIT import *
+
+
+
+# Enable x64 Precision for Jax
+jax.config.update('jax_enable_x64', True)
+#%%
+from matplotlib import pyplot as plt
+
+#%%
+NX = 128
+
+# Domain length
+Lx = 2 * np.pi
+
+# Filter Width
+Delta = 2 * Lx / NX
+
+# Mesh size
+dx = Lx / NX
+
+# Mesh points in x direction
+x = np.linspace(0, Lx - dx, num=NX)
+
+INDEXING = 'ij'
+# -------------- Create the meshgrid both in physical and spectral space --------------
+# Kx, Ky, Ksq = initialize_wavenumbers_2DFHIT(NX, NX, Lx, Lx)
+nx = NX
+ny = NX
+Lx, Ly = 2 * np.pi, 2 * np.pi
+kx = 2 * np.pi * np.fft.fftfreq(nx, d=Lx/nx)
+ky = 2 * np.pi * np.fft.fftfreq(ny, d=Ly/ny)
+(Kx, Ky) = np.meshgrid(kx, ky, indexing=INDEXING)
+Ksq = Kx ** 2 + Ky ** 2
+    #$$
+Kx = np.array(Kx)
+Ky = np.array(Ky)
+Ksq = np.array(Ksq)
+
+# Mesh points in x and y direction
+X, Y = nnp.meshgrid(x, x, indexing =INDEXING)
+
+X = np.array(X)
+Y = np.array(Y)
+#%%
+W = np.sin(X)+np.cos(Y)
+W_hat = np.fft.fft2(W)
+DWDX_exact = np.cos(X)
+DWDY_exact = -np.sin(Y)
+#%%
+plt.contourf(X,Y,W,cmap='bwr');plt.colorbar()
+#%%
+DWDX = np.real(np.fft.ifft2(derivative_2DFHIT(W_hat, [1,0], Kx, Ky)))
+DWDY = np.real(np.fft.ifft2(derivative_2DFHIT(W_hat, [0,1], Kx, Ky)))
+
+#%%
+aa = DWDY# DWDX
+aa_exact = DWDY_exact# DWDX_exact
+plt.figure(figsize=(4,10))
+plt.subplot(4,1,1)
+plt.contourf(X,Y,W,cmap='bwr');plt.colorbar()
+plt.subplot(4,1,2)
+plt.contourf(X,Y,aa_exact,cmap='bwr');plt.colorbar()
+plt.subplot(4,1,3)
+plt.contourf(X,Y,aa,cmap='bwr');plt.colorbar()
+plt.subplot(4,1,4)
+plt.contourf(X,Y,aa-aa_exact,cmap='bwr');plt.colorbar()
+#%%
+# plt.contourf(X)
