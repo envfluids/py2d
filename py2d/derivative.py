23f58bd36266d0bf09bace23cba85293f5625d98
jakharkaran <jakharkaran94@gmail.com>
2023-07-19 17:16:12 -0500

    included functions to calculate derivatives in physical and spectral domains seperately

--- py2d/derivative.py
+++ py2d/derivative.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-def derivative_2D_FHIT(T, order, Kx, Ky, spectral=False):
+def derivative_2DFHIT(T, order, Kx, Ky, spectral=False):
     """
     Calculate spatial derivatives for 2D_FHIT in spectral space.
     Boundary conditions are periodic in x and y spatial dimensions
-    Length of domain 2*pi
 
     Parameters:
     ----------
@@ -25,9 +24,33 @@ def derivative_2D_FHIT(T, order, Kx, Ky, spectral=False):
 
     # If the input data is not in the spectral domain, we transform it
     if spectral == False:
-        T_hat = np.fft.fft2(T)
+        Tderivative = derivative_2DFHIT_spectral(T, order, Kx, Ky)
+        return Tderivative
     else:  # if it is already in the spectral domain, we do nothing
         T_hat = T
+        Tderivative_hat = derivative_2DFHIT_spectral(T_hat, order, Kx, Ky)
+        return Tderivative_hat
+
+    
+def derivative_2DFHIT_spectral(T_hat, order, Kx, Ky):
+    """
+    Calculate spatial derivatives for 2D_FHIT in spectral space.
+    Boundary conditions are periodic in x and y spatial dimensions
+
+    Parameters:
+    ----------
+    T_hat : numpy.ndarray
+        Input flow field. T_hat is in the spectral domain.
+    order : list
+        Array of order of derivatives in x and y spatial dimensions. Expects a list of two integers >=0.
+    Kx, Ky : numpy.ndarray
+        Precomputed wavenumbers in the x and y dimensions.
+
+    Returns:
+    -------
+    Tderivative_hat : numpy.ndarray
+        The derivative of the flow field T_hat in the spectral domain.
+    """
 
     # Orders of derivatives in x and y dimensions
     orderX = order[0]
@@ -36,10 +59,39 @@ def derivative_2D_FHIT(T, order, Kx, Ky, spectral=False):
     # Calculating derivatives in spectral space using the Fourier derivative theorem
     Tderivative_hat = ((1j*Kx)**orderX) * ((1j*Ky)**orderY) * T_hat
 
-    # If the input data was not in the spectral domain, we transform the result back into the physical domain
-    if spectral == False:
-        Tderivative = np.real(np.fft.ifft2(Tderivative_hat))
-        return Tderivative
-    else:  # if it was in the spectral domain, we leave the result in the spectral domain
-        return Tderivative_hat
+    return Tderivative_hat
+
+def derivative_2DFHIT_physical(T, order, Kx, Ky, ):
+    """
+    Calculate spatial derivatives for 2D_FHIT in physical space using spectral methods.
+    Boundary conditions are periodic in x and y spatial dimensions
+
+    Parameters:
+    ----------
+    T : numpy.ndarray
+        Input flow field. T is in the physical domain.
+    order : list
+        Array of order of derivatives in x and y spatial dimensions. Expects a list of two integers >=0.
+    Kx, Ky : numpy.ndarray
+        Precomputed wavenumbers in the x and y dimensions.
+
+    Returns:
+    -------
+    Tderivative : numpy.ndarray
+        The derivative of the flow field T in the physical domain.
+    """
+
+    # We transform data to spectral domain
+    T_hat = np.fft.fft2(T)
+
+    # Orders of derivatives in x and y dimensions
+    Tderivative_hat = derivative_2DFHIT_spectral(T_hat, order, Kx, Ky)
+
+    # Transform the result back into the physical domain
+    Tderivative = np.real(np.fft.ifft2(Tderivative_hat))
+    
+    return Tderivative
+
+    
+
 
