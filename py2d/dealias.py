bf878abb2c325461ffcf4d5bf421f0c1c0255d97
jakharkaran <jakharkaran94@gmail.com>
2024-03-19 20:59:05 -0500

    added code to make padded array for dealiasing conjugate symmetric

--- py2d/dealias.py
+++ py2d/dealias.py
@@ -89,8 +89,6 @@ def padding_for_dealias(u, spectral=False, K=3/2):
     # Scale the spectral data to account for the increased grid size
     u_hat_alias_scaled = K**2 * u_hat_alias
 
-    # ********** Numpy Code ************
-
     # Initialize a 2D array of zeros with the dealiased shape
     u_hat_dealias = np.zeros((N_dealias, N_dealias), dtype=complex)
 
@@ -100,38 +98,40 @@ def padding_for_dealias(u, spectral=False, K=3/2):
     # Apply scaling and pad the spectral data with zeros
     u_hat_dealias[np.ix_(indvpad, indvpad)] = u_hat_alias_scaled
 
-    # u_hat_dealias[int(N_alias/2)+1,:] = 0
-    # u_hat_dealias[:,int(N_alias/2)+1] = 0
+    # Making the padded array conjugate symmetric
+    u_hat_dealias_pad = conjugate_symmetrize_padding(u_hat_dealias.copy())
 
     if spectral:
-        return u_hat_dealias
+        return u_hat_dealias_pad
     else:
         # Return in the appropriate space
-        return np.fft.ifft2(u_hat_dealias).real
-
-@jit
-def multiply_dealias_physical_jit(u, v):
-    """
-    Multiply two fields and dealias the result, Input is in spectral space and returned in physical space.
-
-    Parameters:
-    - u_hat, v_hat: 2D numpy arrays representing the spectral coefficients of the fields.
-
-    Returns:
-    - Product of the two fields, dealiased and converted to physical space.
-    """
-
-    u_hat = jnp.fft.fft2(u)
-    v_hat = jnp.fft.fft2(v)
-
-    uv_dealias_hat = multiply_dealias_spectral_jit(u_hat, v_hat)
-
-    uv_dealias = jnp.fft.ifft2(uv_dealias_hat).real
+        return np.fft.ifft2(u_hat_dealias_pad).real
     
-    return uv_dealias
+def conjugate_symmetrize_padding(a_hat_fft_pad):
+    
+    N_dealias = int(a_hat_fft_pad.shape[0])
+
+    a_hat_fft_pad_sym = a_hat_fft_pad.copy()
+
+    # # First Making first row and column conjugate symmetric (0th wavenumber data)
+    # a_hat_fft_pad_sym[0,N_dealias//2+1] = np.conj(a_hat_fft_pad_sym[0,N_dealias//2-1])
+    # a_hat_fft_pad_sym[N_dealias//2+1,0] = np.conj(a_hat_fft_pad_sym[N_dealias//2-1,0])
+
+    # # Conjugate symmetry of 'N_dealias//2-1 'th' row and column to 'N_dealias//2+1' 'th' row and column
+    # # Uneven padding of zeros creates this error
+    # a_hat_fft_pad_sym[-(N_dealias//2-1),1:] = np.conj(np.flip(a_hat_fft_pad_sym[(N_dealias//2-1),1:]))
+    # a_hat_fft_pad_sym[1:,-(N_dealias//2-1)] = np.conj(np.flip(a_hat_fft_pad_sym[1:,(N_dealias//2-1)]))
+
+    ############ Alternatively equate the data to zero at wavenumber (N//2-1) to (N//2+1) to make it conjugate symmetric
+    a_hat_fft_pad_sym[int(N_dealias/2)-1,:] = 0
+    a_hat_fft_pad_sym[:,int(N_dealias/2)-1] = 0
+    a_hat_fft_pad_sym[int(N_dealias/2)+1,:] = 0
+    a_hat_fft_pad_sym[:,int(N_dealias/2)+1] = 0
+
+    return a_hat_fft_pad_sym
 
 @jit
-def multiply_dealias_spectral_jit(u_hat, v_hat):
+def multiply_dealias_physical_jit(a, b):
     """
     Multiply two fields and dealias the result, Input is in spectral space and returned in physical space.
 
@@ -142,24 +142,45 @@ def multiply_dealias_spectral_jit(u_hat, v_hat):
     - Product of the two fields, dealiased and converted to physical space.
     """
 
-    Ncoarse = u_hat.shape[0]
+    a_hat = jnp.fft.fft2(a)
+    b_hat = jnp.fft.fft2(b)
+
+    ab_dealias_hat = multiply_dealias_spectral_jit(a_hat, b_hat)
+
+    ab_dealias = jnp.fft.ifft2(ab_dealias_hat).real
+    
+    return ab_dealias
+
+@jit
+def multiply_dealias_spectral_jit(a_hat, b_hat):
+    """
+    Multiply two fields and dealias the result, Input is in spectral space and returned in physical space.
+
+    Parameters:
+    - u_hat, v_hat: 2D numpy arrays representing the spectral coefficients of the fields.
+
+    Returns:
+    - Product of the two fields, dealiased and converted to physical space.
+    """
+
+    Ncoarse = a_hat.shape[0]
 
     # Dealias each field
-    u_dealias_hat = padding_for_dealias_spectral_jit(u_hat)
-    v_dealias_hat = padding_for_dealias_spectral_jit(v_hat)
+    a_dealias_hat = padding_for_dealias_spectral_jit(a_hat)
+    b_dealias_hat = padding_for_dealias_spectral_jit(b_hat)
 
-    u_dealias = jnp.fft.ifft2(u_dealias_hat).real
-    v_dealias = jnp.fft.ifft2(v_dealias_hat).real
+    a_dealias = jnp.fft.ifft2(a_dealias_hat).real
+    b_dealias = jnp.fft.ifft2(b_dealias_hat).real
 
-    u_dealias_v_dealias_hat = jnp.fft.fft2(u_dealias * v_dealias)
+    a_dealias_b_dealias_hat = jnp.fft.fft2(a_dealias * b_dealias)
 
     # Multiply on the dealise grid and coarse grain to alias grid
-    uv_dealias_hat = coarse_spectral_filter_square_2DFHIT_jit(u_dealias_v_dealias_hat, Ncoarse)
+    ab_dealias_hat = coarse_spectral_filter_square_2DFHIT_jit(a_dealias_b_dealias_hat, Ncoarse)
     
-    return uv_dealias_hat
+    return ab_dealias_hat
 
 @jit
-def padding_for_dealias_spectral_jit(u_hat_alias, K=3/2):
+def padding_for_dealias_spectral_jit(a_hat_alias, K=3/2):
     """
     Padding zeros to the high wavenumber in the
     spectral space for a 2D square grid. This is required for handling
@@ -178,27 +199,56 @@ def padding_for_dealias_spectral_jit(u_hat_alias, K=3/2):
     - The u_hat_dealias needs to be conjugate symmetric for the inverse FFT to be real.
     """
     # Determine the original and dealiased grid sizes
-    N_alias = u_hat_alias.shape[0]
+    N_alias = a_hat_alias.shape[0]
     N_dealias = int(K * N_alias)
 
     # Scale the spectral data to account for the increased grid size
-    u_hat_alias_scaled = K**2 * u_hat_alias
+    a_hat_alias_scaled = K**2 * a_hat_alias
 
     # ********** Jax Code ************
 
-    u_hat_dealias = jnp.zeros((N_dealias, N_dealias), dtype=complex)
+    a_hat_dealias = jnp.zeros((N_dealias, N_dealias), dtype=complex)
 
   # Extract the corners of the scaled array
-    utopleft = u_hat_alias_scaled[0:int(N_alias/2)+1, 0:int(N_alias/2)+1]
-    utopright = u_hat_alias_scaled[0:int(N_alias/2)+1, N_alias-int(N_alias/2)+1:N_alias]
-    ubottomleft = u_hat_alias_scaled[N_alias-int(N_alias/2)+1:N_alias, 0:int(N_alias/2)+1]
-    ubottomright = u_hat_alias_scaled[N_alias-int(N_alias/2)+1:N_alias, N_alias-int(N_alias/2)+1:N_alias]
+    utopleft = a_hat_alias_scaled[0:int(N_alias/2)+1, 0:int(N_alias/2)+1]
+    utopright = a_hat_alias_scaled[0:int(N_alias/2)+1, N_alias-int(N_alias/2)+1:N_alias]
+    ubottomleft = a_hat_alias_scaled[N_alias-int(N_alias/2)+1:N_alias, 0:int(N_alias/2)+1]
+    ubottomright = a_hat_alias_scaled[N_alias-int(N_alias/2)+1:N_alias, N_alias-int(N_alias/2)+1:N_alias]
 
     # Since JAX arrays are immutable, use the .at[].set() method for updates
-    u_hat_dealias = u_hat_dealias.at[0:int(N_alias/2)+1, 0:int(N_alias/2)+1].set(utopleft)
-    u_hat_dealias = u_hat_dealias.at[0:int(N_alias/2)+1, N_dealias-int(N_alias/2)+1:N_dealias].set(utopright)
-    u_hat_dealias = u_hat_dealias.at[N_dealias-int(N_alias/2)+1:N_dealias, 0:int(N_alias/2)+1].set(ubottomleft)
-    u_hat_dealias = u_hat_dealias.at[N_dealias-int(N_alias/2)+1:N_dealias, N_dealias-int(N_alias/2)+1:N_dealias].set(ubottomright)
-    
+    a_hat_dealias = a_hat_dealias.at[0:int(N_alias/2)+1, 0:int(N_alias/2)+1].set(utopleft)
+    a_hat_dealias = a_hat_dealias.at[0:int(N_alias/2)+1, N_dealias-int(N_alias/2)+1:N_dealias].set(utopright)
+    a_hat_dealias = a_hat_dealias.at[N_dealias-int(N_alias/2)+1:N_dealias, 0:int(N_alias/2)+1].set(ubottomleft)
+    a_hat_dealias = a_hat_dealias.at[N_dealias-int(N_alias/2)+1:N_dealias, N_dealias-int(N_alias/2)+1:N_dealias].set(ubottomright)
+
+    # Making first row,col conjugate symmetric as well as the Nyquist +/- 1 row,col
+    a_hat_dealias_pad = conjugate_symmetrize_padding_jit(a_hat_dealias.copy())
+
     # Return in the appropriate space
-    return u_hat_dealias
+    return a_hat_dealias_pad
+
+@jit
+def conjugate_symmetrize_padding_jit(a_hat_fft_pad):
+
+    N_dealias = int(a_hat_fft_pad.shape[0])
+    a_hat_fft_pad_sym = a_hat_fft_pad.copy()
+
+    # # First Row and Column (using at.set)
+    # a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[0, N_dealias//2+1].set(jnp.conj(a_hat_fft_pad_sym[0, N_dealias//2-1]))
+    # a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[N_dealias//2+1, 0].set(jnp.conj(a_hat_fft_pad_sym[N_dealias//2-1, 0]))
+
+    # # Conjugate Symmetry with Slicing and Vectorization
+    # # Calculate slice indices dynamically
+    # slice_start = N_dealias // 2 - 1
+    # slice_end = -slice_start
+
+    # a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[slice_end, 1:].set(jnp.conj(jnp.flip(a_hat_fft_pad_sym[slice_start, 1:])))
+    # a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[1:, slice_end].set(jnp.conj(jnp.flip(a_hat_fft_pad_sym[1:, slice_start])))
+
+    ############ Alternatively equate the data to zero at wavenumber (N//2-1) to (N//2+1) to make it conjugate symmetric
+    a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[N_dealias//2-1,:].set(0)
+    a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[N_dealias//2+1,:].set(0)
+    a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[:,N_dealias//2-1].set(0)
+    a_hat_fft_pad_sym = a_hat_fft_pad_sym.at[:,N_dealias//2+1].set(0)
+
+    return a_hat_fft_pad_sym
